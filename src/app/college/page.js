fe0fea0f61e1c5b08805c8205c31ba856aaec9f7
sameerishaare <sameergupta4873@gmail.com>
2024-09-06 23:32:31 +0530

    Update page.js

--- src/app/college/page.js
+++ src/app/college/page.js
@@ -1194,7 +1194,7 @@ const Users = () => {
               </thead>
               <tbody>
                 {students ? (
-                  students.map((student,ix) => {
+                  students.map((student,idx) => {
                     return (
                       <tr key={idx} class="bg-white border-b# dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                         <td class="w-4 p-4">
